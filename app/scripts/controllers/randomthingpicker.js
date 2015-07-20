b0ce56cdbe1f4a19a4e9a897eb0af2c4d224de1f
kelvinatorr <kelvin.dealca@gmail.com>
2015-07-19 20:51:07 -0700

    add randomthingpicker controller

---
+++ app/scripts/controllers/randomthingpicker.js
@@ -0,0 +1,19 @@
+(function(){
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name hotpotApp.controller:RandomThingPickerCtrl
+   * @description
+   * # RandomThingPickerCtrl
+   * Controller of the hotpotApp
+   */
+  angular.module('hotpotApp')
+    .controller('RandomThingPickerCtrl', RandomThingPickerCtrl);
+
+  function RandomThingPickerCtrl() {
+    
+  }
+
+})();
